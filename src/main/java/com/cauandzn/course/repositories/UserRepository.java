6f84a9b3c850aff81e080b560d9687a01e2a7717
Cauan <cauanvictor0325@gmail.com>
2024-09-22 13:37:15 -0300

    Service layer, component registration

--- src/main/java/com/cauandzn/course/repositories/UserRepository.java
+++ src/main/java/com/cauandzn/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.cauandzn.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cauandzn.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
